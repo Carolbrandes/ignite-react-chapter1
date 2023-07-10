a7bc875b1b41225829ecb7aafa625d2e741a8787
carol <carolbrandes23@gmail.com>
2023-07-10 06:52:12 -0300

    config webpack for generate index.html in dist, for don't need to reference the bundle.js manually.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path"); // * para colocar o caminho de acordo com sistema operacional
-
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // * para nao precisar colocar no index.html a chamada do script da pasta bundle (p nao ter q lidar com possiveis alteracoes do caminho do arquivo) geramos no dist o arquivo html tb q ja chama o arquivo bundle.js
 module.exports = {
   mode: "development",
   entry: path.resolve(__dirname, "src", "index.jsx"),
@@ -10,6 +10,11 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"], // nos arquivos jsx, js qd fizemos uma importacao de outro arquivo nao precisamos botar a extensao dele por conta dessa config
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
   module: {
     rules: [
       {
